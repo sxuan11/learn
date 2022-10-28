e63ebaf0bbbb08347110208ab81aec0af0f7f082
sxuan <zhangshengxuan@polyv.net>
2022-10-28 12:00:17 +0800

    fix 修正GitHub pages问题

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -25,4 +28,19 @@ export default defineConfig({
     ],
   },
   base: '/learn/',
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(fileName) {
+          const match = DRIVE_LETTER_REGEX.exec(fileName);
+          const driveLetter = match ? match[0] : "";
+          return (
+            driveLetter +
+            fileName.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
+  }
 })
